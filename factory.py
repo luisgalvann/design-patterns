f424b3e025769fd4008ae9a3ad66b113a09d790d
luis <luis@gmail.com>
2021-10-14 12:43:49 +0200

    Inicio repositorio

---
+++ factory.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    '''Abstract Shape «interface»'''
+
+    @staticmethod  # This way it doesn't need 'self'
+    @abstractmethod
+    def draw() -> None:
+        raise NotImplementedError
+
+
+class Circle(Shape):
+    '''Concrete Shape Circle'''
+
+    def draw(self) -> None:
+        print('drawing a circle')
+
+
+class Square(Shape):
+    '''Concrete Shape Square'''
+
+    def draw(self) -> None:
+        print('drawing a square')
+
+
+class Rectangle(Shape):
+    '''Concrete Shape Rectangle'''
+
+    def draw(self) -> None:
+        print('drawing a rectangle')
+
+
+class ShapeFactory:
+    '''ShapeFactory Implementation'''
+    
+    @staticmethod
+    def getShape(choice: str) -> Shape:
+        shape = {
+            'circle': Circle(),
+            'square': Square(),
+            'rectangle': Rectangle(),
+        }.get(choice)
+
+        return shape
